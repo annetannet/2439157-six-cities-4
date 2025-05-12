c4eae20095b5b8154fe742e8695a55042bb92213
annetannet <annetpeach@example.com>
2025-05-12 23:40:07 +0500

    delete files

--- src/shared/types/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './offer.type.js';
-export * from './user.type.js';
-export * from './mock-server-data.type.js';
-export * from './offer-type.enum.js';
-export * from './component.enum.js';
-export * from './category.type.js';
